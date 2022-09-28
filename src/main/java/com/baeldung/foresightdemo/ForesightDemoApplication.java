e195ed8e5361092e58b3d301ed524df62ab7d2df
Graham Cox <graham@grahamcox.co.uk>
2022-09-28 07:34:51 +0100

    Initial commit from Spring Initializr

---
+++ src/main/java/com/baeldung/foresightdemo/ForesightDemoApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.foresightdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForesightDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForesightDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeldung/foresightdemo/ForesightDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.foresightdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForesightDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
