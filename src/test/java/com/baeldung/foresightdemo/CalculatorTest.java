86cf4f569d36d47fcb0f5593d291f23ca352838b
Graham Cox <graham@grahamcox.co.uk>
2022-10-11 07:35:19 +0100

    Added a slow and a randomly failing test

--- src/test/java/com/baeldung/foresightdemo/CalculatorTest.java
+++ src/test/java/com/baeldung/foresightdemo/CalculatorTest.java
@@ -2,6 +2,8 @@ package com.baeldung.foresightdemo;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.SecureRandom;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -10,5 +12,23 @@ class CalculatorTest {
     @Test
     void testAdd() {
         assertEquals(3, calculator.add(1, 2));
+    }
+
+    @Test
+    void testFailing() {
+        var rand = new SecureRandom();
+        var value = rand.nextDouble();
+
+        if (value > 0.5) {
+            fail();
+        }
+    }
+
+    @Test
+    void testSlow() throws InterruptedException {
+        var rand = new SecureRandom();
+        var value = rand.nextInt(10000);
+
+        Thread.sleep(value);
     }
 }
